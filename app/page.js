5dff996d869683e09857e66e1641fb33c524a938
Estéfane Souza <53922973+estefanecs@users.noreply.github.com>
2023-05-13 09:38:32 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,95 +1,76 @@
+"use client"; 
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import React from 'react';
+import { useState } from 'react';
+
+
+function SearchBar({filterText, inStockOnly, onFilterTextChange,
+  onInStockOnlyChange}) {
+  return (
+    <form>
+      <label>
+        Procure uma mensagem:
+      </label>
+      <input type="text" value={filterText} placeholder="Search..." 
+      onChange={(e) => onFilterTextChange(e.target.value)}/>
+    </form>
+  );
+}
+
+function FilterableMessageTable({ messages, filterText}) {
+  const rows = [];
+  messages.forEach((message) => {
+     if (messages.includes(filterText)) {
+      return;
+    }
+    rows.push(
+      <MessageRow
+        message={message}
+        key={message.message} />);
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Author</th>
+          <th>Message</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function MessageRow({ message }) {
+  return (
+    <tr>
+      <td>{message.author}</td>
+      <td>{message.message}</td>
+      <td>{message.date}</td>
+    </tr>
+  );
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    
+  const [blogMessages, setBlogMessages] = useState([]);
+  
+  fetch('https://script.google.com/macros/s/AKfycbzBn3sALe1rYjz7Ze-Ik7q9TEVP0I2V3XX7GNcecWP8NvCzGt4yO_RT1OlQp09TE9cU/exec')
+    .then(response => response.json())
+    .then(data => {
+        setBlogMessages(data);
+    });
+    
+    return (
+      <main className={styles.main}>
+         <SearchBar filterText={filterText}  inStockOnly={inStockOnly} 
+      onFilterTextChange={setFilterText}
+      onInStockOnlyChange={setInStockOnly}/>
+        <FilterableMessageTable messages={blogMessages} filterText={filterText} />
+      </main>
+    );
 }
